2c08b11cd93766c35173d49a1f4986b287adef17
Deanisgarayev <garaevdenis2019@gmail.com>
2024-01-13 20:39:36 +0500

    Beginning main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import java.time.format.DateTimeFormatter;
+
+import java.util.List;
+
 public class Main {
+    static DateTimeFormatter dateAndTime = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Flight> flightList = FlightBuilder.createFlights();
+
+        System.out.println("Исходные перелёты, полученные из createFlights():");
+        flightList.forEach(System.out::println);
+
+        System.out.println("Вылет до текущего момента времени");
+        departureBeforeCurrentTimeFilter(flightList).forEach(System.out::println);
+
+        System.out.println("Сегменты с датой прилёта раньше даты вылета");
+        segmentsWithDateArrivalEarlierThanDepartureFilter(flightList).forEach(System.out::println);
+
+        System.out.println("Перелеты, где общее время, проведённое на земле, превышает два часа " +
+                "(время на земле — это интервал между прилётом одного сегмента и вылетом следующего за ним)");
+        flightsWhereMoreThenTwoHoursOnGroundFilter(flightList).forEach(System.out::println);
+    }
+
+    public static List<Flight> departureBeforeCurrentTimeFilter(List<Flight> flightList) {
+        return flightList;
+    }
+
+    public static List<Flight> segmentsWithDateArrivalEarlierThanDepartureFilter(List<Flight> flightList) {
+        return flightList;
+    }
+
+    public static List<Flight> flightsWhereMoreThenTwoHoursOnGroundFilter(List<Flight> flightList) {
+        return flightList;
     }
 }
