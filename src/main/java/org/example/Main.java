7bea5c6697896f9a497329707d465813f9465351
Deanisgarayev <garaevdenis2019@gmail.com>
2024-01-16 23:35:25 +0500

    i finished it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,9 @@
 package org.example;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,40 +1,39 @@
 import org.example.Flight;
+import org.example.FlightBuilder;
 import org.example.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class MainTest {
-    private static final Main out = new Main();
 
-
-
+    List<Flight> flightList1 = FlightBuilder.createFlights();
     @Test
     public void departureBeforeCurrentTimeFilterTest() {
-        List<Flight> flights = new ArrayList<>();
+        List<Flight> flights = new ArrayList<>(flightList1);
         List<Flight> flightList = Main.departureBeforeCurrentTimeFilter(flights);
-        List<Flight> expected = new ArrayList<>();
+        List<Flight> expected = new ArrayList<>(flightList1);
+        expected.remove(flightList1.get(2));
         Assertions.assertIterableEquals(expected, flightList);
         Assertions.assertNotNull(flightList);
     }
     @Test
     public void segmentsWithDateArrivalEarlierThanDepartureFilterTest() {
-        List<Flight> flights = new ArrayList<>();
+        List<Flight> flights = new ArrayList<>(flightList1);
         List<Flight> flightList = Main.segmentsWithDateArrivalEarlierThanDepartureFilter(flights);
-        List<Flight> expected = new ArrayList<>();
+        List<Flight> expected = new ArrayList<>(flightList1);
+        expected.remove(flightList1.get(3));
         Assertions.assertIterableEquals(expected, flightList);
         Assertions.assertNotNull(flightList);
     }
     @Test
     public void moreThenTwoHoursWaitingFilterTest() {
-        List<Flight> flights = new ArrayList<>();
+        List<Flight> flights = new ArrayList<>(flightList1);
         List<Flight> flightList =  Main.flightsWhereMoreThenTwoHoursOnGroundFilter(flights);
-        List<Flight> expected = new ArrayList<>();
+        List<Flight> expected = new ArrayList<>(flightList1);
+        expected.remove(flightList1.get(4));
         Assertions.assertIterableEquals(expected, flightList);
         Assertions.assertNotNull(flightList);
     }
